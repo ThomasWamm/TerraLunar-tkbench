dc78884c180137998f6776ffd56272879243cf8f
ThomasW <thomaswamm@gmail.com>
2021-03-30 17:16:13 -0700

    simplified whichTclTk

--- whichTclTk.py
+++ whichTclTk.py
@@ -3,21 +3,18 @@
 import _tkinter
 
 def getver():
-    '''Return a string with Tcl and Tk versions reported by _tkinter.
+    '''Return a string with Tcl and Tk versions as imagined by _tkinter.
     Apparently the most minor revision number is omitted.'''
     tclver = str(_tkinter.TCL_VERSION)
     tkver  = str(_tkinter.TK_VERSION)
-    versions = "Tcl " + tclver + " Tk " + tkver
-    return versions
+    return "Tcl " + tclver + " Tk " + tkver
 
 def getpatch():
     '''Return a string with Tcl/Tk patchlevel.'''
     tk = _tkinter.create()
-    patchlevel  = str(tk.call("info", "patchlevel"))
-    #del tk      # I'm not sure this is a good idea.
-    return "Tcl/Tk " + patchlevel
+    return "Tcl/Tk " + str(tk.call("info", "patchlevel"))
 
-# Test:
+# uncomment for testing:
 '''
 print("    Tcl/Tk versions = " + getver())
 print("    Tcl/Tk patchlevel = " + getpatch())
